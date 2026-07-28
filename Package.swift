1d0461f61ae3f818c025f889352f488c450664ee
Vitor Souza <vitor.souza@oititec.com.br>
2026-07-28 10:08:10 -0300

    chore: update framework and versions

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/oititec/ios-liveness3d-ft.git",
-            exact: "9.7.121"
+            exact: "9.7.130"
         )
     ],
     targets: [
